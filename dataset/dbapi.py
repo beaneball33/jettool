f933917454fae8b3201b3ad9d1cf631330dee40d
stanleyafu <58063191+stanleyafu@users.noreply.github.com>
2020-03-24 17:16:12 +0800

    新增dbapi及finreport文字說明 (#5)

--- dataset/dbapi.py
+++ dataset/dbapi.py
@@ -23,6 +23,7 @@ def get_info():
         
     print(print_info)
     return info  
+
 # 取得按照索引目錄分層的完整資料表清單
 def set_tablelist(tables:list):
         
@@ -37,7 +38,8 @@ def set_tablelist(tables:list):
             api_tables[market].append(table)
 
     return api_tables
-# 查詢所有的國別    
+
+# 查詢所有的國別的資料庫資訊  
 def get_market():
 
     db_names = ('https://api.tej.com.tw/info/database/list?api_key='
@@ -46,17 +48,19 @@ def get_market():
     data = json.loads(response.text)['result']
     market_list = data
     return market_list
-# 查詢所有資料分類    
-def get_category():
 
-        
+# 查詢所有資料分類的資料庫    
+def get_category():
+       
     list_names = ('https://api.tej.com.tw/info/category/list?api_key='
                   +api_key)
     response = requests.get(list_names)
     data = json.loads(response.text)['result']
     category_list = { data[k].get('categoryId'):data[k] for k in data}
     return category_list
+
 # 查詢按國別分類的資料表完整清單   
+# 查詢所有資料表資訊內容
 def get_tables():
 
     table_names = ('https://api.tej.com.tw/info/tables/list?api_key='
@@ -92,8 +96,8 @@ def get_tables():
         table_list[market_code] = this_market_table
     return table_list
 
-# 根據索引目錄的資料表清單構造，回傳dataframe方便檢視
-def get_tables_info(*,market:str = 'TWN',table_list:list = {}):            
+# 根據索引目錄的資料表清單構造，回傳 dataframe方便檢視
+def get_tables_info(*, market:str = 'TWN', table_list:list = {}):            
     df = None
     
     # 把所有國別資料表都回傳
@@ -108,17 +112,18 @@ def get_tables_info(*,market:str = 'TWN',table_list:list = {}):
                                 table_list.get(market),
                                             orient='index'),sort=False)
         return df 
+    
     # 僅回傳指定國別
     else:
         df = pandas.DataFrame.from_dict(table_list.get(market),orient='index')
          
-    return df.rename(columns={'id':'資料集名稱','dbCode':'國別碼',
-                              'tableCode':'資料表代碼','name':'名稱',
-                              'description':'描述','enabled':'存取權限',
+    return df.rename(columns={'id':'資料集名稱', 'dbCode':'國別碼',
+                              'tableCode':'資料表代碼', 'name':'名稱',
+                              'description':'描述', 'enabled':'存取權限',
                               'frequency':'頻率'})
 
 
-# 根據已知的table名稱，查詢mapping到別的國家的資料表清單
+# 根據已知的table名稱，查詢 mapping到別的國家的資料表清單
 def get_table_mapping(table_name:str = 'TWN/AIND',*,
                       category_list:list = None):
     market = table_name.split('/')[0]
@@ -132,9 +137,11 @@ def get_table_mapping(table_name:str = 'TWN/AIND',*,
                 if (market in table.get('dbCode') and
                     table_name in table.get('tableId')):
                     return tableMap
+                
 # 使用tejapi.search_table進行交集或聯集查詢
-def search_column(*,market:str = 'TWN',keyword:str = '報酬率',
-                    condition:str = 'and',current_market=True):
+# 輸入要搜尋的關鍵字，並且產出完整會計科目的中文名稱                    
+def search_column(*, market:str = 'TWN', keyword:str = '報酬率',
+                    condition:str = 'and', current_market=True):
 
     tejapi.ApiConfig.api_key = api_key
     k_name_list = keyword.split(' ')
@@ -143,7 +150,7 @@ def search_column(*,market:str = 'TWN',keyword:str = '報酬率',
     # 先查出第一個關鍵字的搜尋結果
     match_dict = { search['tableId']:search for search in tejapi.search_table(k_name)}
     match_outcome = []
-    if len(match_dict)>0:
+    if len(match_dict) > 0:
         # 逐個搜索結果表單檢查
         for match_table in match_dict:
             # 逐欄進行名稱檢查
@@ -163,17 +170,17 @@ def search_column(*,market:str = 'TWN',keyword:str = '報酬率',
     match_df = pandas.DataFrame(match_outcome,
                                 columns=['cname','tableName','tableCode'])
     if condition =='and':
-        for i in range(1,len(k_name_list)):
-            if len(match_df)>0:
-                for j,row in match_df.iterrows():
+        for i in range(1, len(k_name_list)):
+            if len(match_df) > 0:
+                for j, row in match_df.iterrows():
                     if k_name_list[i] not in match_df.loc[j,'cname']:
                         match_df.loc[j,'tableCode'] = None
     match_df = match_df.dropna().reset_index(drop=True)
     current_market
     return match_df
     
-# 取得指定表單的完整欄位表
-def get_table_columns(*,table_name:str = 'TWN/AAPRCDA'):
+# 取得指定資料庫的完整資料庫欄位表
+def get_table_columns(*, table_name:str = 'TWN/AAPRCDA') -> list:
 
     tejapi.ApiConfig.api_key = api_key
     columns_name = []
--- dataset/finreport.py
+++ dataset/finreport.py
@@ -14,44 +14,50 @@ api_key=''
 
 def set_params(new_params):
     for param in new_params:
-        #只取用新dict中是普通參數的部分
-        if '__' not in param and not callable(new_params.get(param)):   
-            #只更新兩者共通參數
+        # 只取用新dict中是普通參數的部分
+        if '__' not in param and not callable(new_params.get(param)):  
+            
+            # 只更新兩者共通參數
             if params.__dict__.get(param) is not None:
                 params.__dict__[param] = new_params.get(param)
-           
-def inital_report(*,code_table:str = 'TWN/AIACC',
+                
+# 找出會計代碼對照表，產出 accountData           
+def inital_report(*, code_table:str = 'TWN/AIACC',
                     actvie_code_table:str = 'TWN/AINVFACC_INFO_C'):
 
     if params.accountData is 'na':
         tejapi.ApiConfig.api_key = api_key
         params.accountData = tejapi.get(code_table)
         params.activeAccountData = tejapi.get(actvie_code_table)
-        params.accountData['cname'] = params.accountData['cname'].str.replace('(','（').replace(')','）')
+        params.accountData['cname'] = params.accountData['cname'].str.replace('(', '（').replace(')', '）')
         params.accountData = params.accountData.sort_values(by=['cname'])
-        params.accountData = params.accountData.drop_duplicates(subset=['code'],keep='last')
+        params.accountData = params.accountData.drop_duplicates(subset=['code'], keep='last')
 
-def get_by_word(keyword:str = '損益',*,
+# 查詢包含特定文字的會計科目名稱
+def get_by_word(keyword:str = '損益', *,
                 active_view:bool = False):
 
     inital_report()
     if active_view is False:
-        ans = params.accountData.loc[params.accountData['cname'].str.contains(keyword),'cname'].reset_index(drop=True)
+        ans = params.accountData.loc[params.accountData['cname'].str.contains(keyword), 'cname'].reset_index(drop=True)
     else:
-        ans = params.activeAccountData.loc[params.activeAccountData['cdesc'].str.contains(keyword),'cdesc'].reset_index(drop=True)
+        ans = params.activeAccountData.loc[params.activeAccountData['cdesc'].str.contains(keyword), 'cdesc'].reset_index(drop=True)
     return ans
-def get_by_cgrp(cgrp:list = ['損益表'],*,active_view:bool = False):
+
+# 查詢財務報表中所有的會計科目名稱
+def get_by_cgrp(cgrp:list = ['損益表'], *, active_view:bool = False):
 
     inital_report()
     if active_view is False:
-        ans = params.accountData.loc[params.accountData['cgrp'].isin(cgrp),'cname'].reset_index(drop=True)
+        ans = params.accountData.loc[params.accountData['cgrp'].isin(cgrp), 'cname'].reset_index(drop=True)
     else:
-        cgrp_list = {'非經常性損益':'X','比率':'R','損益表':'I','現金流量表':'C','資產負債表':'B','比率計算':'Z'}
+        cgrp_list = {'非經常性損益':'X', '比率':'R', '損益表':'I', '現金流量表':'C', '資產負債表':'B', '比率計算':'Z'}
         cgrp_code = [cgrp_list.get(cgrp[k]) for k in range(0,len(cgrp))]
-        ans = params.activeAccountData.loc[params.activeAccountData['acct_type'].isin(cgrp_code),'cdesc'].reset_index(drop=True)
+        ans = params.activeAccountData.loc[params.activeAccountData['acct_type'].isin(cgrp_code), 'cdesc'].reset_index(drop=True)
     return ans
-    
-def get_acc_code(acc_name:list = [],*,active_view:bool = False):
+
+# 輸入:acc_name(會計科目中文名稱)，可以獲得會計科目代碼(query_code)     
+def get_acc_code(acc_name:list = [], *, active_view:bool = False):
 
     tejapi.ApiConfig.api_key = api_key
 
@@ -59,44 +65,48 @@ def get_acc_code(acc_name:list = [],*,active_view:bool = False):
     ans = []
     acc_name_set = set(acc_name)
     acc_name = list(acc_name_set)
-    if len(acc_name)>0:
+    if len(acc_name) > 0:
         for cname in acc_name:
             if active_view is False:
                 params.accountData['target'] = cname
-                this_code = params.accountData.loc[params.accountData['cname']==params.accountData['target'],'code']
+                this_code = params.accountData.loc[params.accountData['cname']==params.accountData['target'], 'code']
             else:
                 params.activeAccountData['target'] = cname
-                this_code = params.activeAccountData.loc[params.activeAccountData['cdesc']==params.activeAccountData['target'],'acct_code']                    
-            if len(this_code)>0:
+                this_code = params.activeAccountData.loc[params.activeAccountData['cdesc']==params.activeAccountData['target'], 'acct_code']                    
+            if len(this_code) > 0:
                 ans += [this_code.values[0]]
             else:
                 if '兌' in cname and '匯率' in cname:
-                    cname = cname.replace('匯率','').replace('兌','@')
+                    cname = cname.replace('匯率', '').replace('兌', '@')
                     ans += [cname]
                 else:
-                    print(cname+':not find')
+                    print(cname + ':not find')
         return ans
-def get_announce(*,table_id:str = None,query_coid:list = [],sample_dates:list = []):
+
+# 查詢指定日期之間的個股財報公布日
+def get_announce(*, table_id:str = None, query_coid:list = [], sample_dates:list = []):
 
     tejapi.ApiConfig.api_key = api_key
     if table_id is None:
         table_id = params.announceTable
     if len(query_coid) == 0:
         query_coid = params.input_coids
-    if len(query_coid)==0 or len(sample_dates)!=2:
+    if len(query_coid) == 0 or len(sample_dates) != 2:
         return None
     datastart_date = sample_dates[0]
     current_zdate = sample_dates[1]
     fin_cover = tejapi.get(table_id,
                            coid=query_coid,
-                           a0003={'gte':datastart_date,'lte':current_zdate},
+                           a0003={'gte':datastart_date, 'lte':current_zdate},
                            opts={'sort':'mdate.desc',
                                  'columns':['coid','mdate','a0003']
-                                 },paginate=True).rename(
+                                 } ,paginate=True).rename(
                            index=str, columns={"a0003": "zdate"})
     return   fin_cover
-def get_report(*,query_code:list = [],query_coid:list = [],
-                 sample_dates:list = [],rename_cols:bool= True):
+
+# 輸入: query_code(會計科目代碼) query_coid  產出: fin_data
+def get_report(*, query_code:list = [], query_coid:list = [],
+                 sample_dates:list = [], rename_cols:bool= True):
     params.acc_code = query_code
 
     tejapi.ApiConfig.api_key = api_key
@@ -104,54 +114,58 @@ def get_report(*,query_code:list = [],query_coid:list = [],
     if len(query_coid) == 0:
         query_coid = params.input_coids
     all_fin_data = None
-    if (len(query_coid)==0 or 
-        len(query_code)==0 or
-        len(sample_dates)!=2):
+    if (len(query_coid) == 0 or 
+        len(query_code) == 0 or
+        len(sample_dates) != 2):
         return None
+    
     #嘗試查詢報表封面
-
     fin_cover = get_announce(query_coid=query_coid,
                              sample_dates=sample_dates)
     fin_data = fin_cover.copy()            
     actual_ciod = fin_data['coid'].unique().tolist()
     datastart_date = sample_dates[0]
     current_zdate = sample_dates[1]
-    if len(fin_data)>0:
+    if len(fin_data) > 0:
         #如果有查到，代表是這個表
         #查詢報表
-        query_column = ['coid','mdate'] + params.acc_code
+        query_column = ['coid', 'mdate'] + params.acc_code
 
-        fin_report  = tejapi.get(params.findataTable,coid=actual_ciod,
-            mdate={'gte':datastart_date,'lte':current_zdate},
-            opts={"sort":"mdate.desc",'columns':query_column,"pivot":True},paginate=True)
-
-        fin_data = fin_data.merge(fin_report,on=['coid','mdate'],how='left').fillna(0)
+        fin_report  = tejapi.get(params.findataTable, coid=actual_ciod,
+            mdate={'gte':datastart_date, 'lte':current_zdate},
+            opts={"sort":"mdate.desc", 'columns':query_column, "pivot":True}, paginate=True)
+        
+        # 合併財務資料及處理日期格式
+        fin_data = fin_data.merge(fin_report, on=['coid', 'mdate'], how='left').fillna(0)
         fin_data['mdate'] =  fin_data['mdate'].astype(str).astype('datetime64')
         fin_data['zdate'] =  fin_data['zdate'].astype(str).astype('datetime64')
+        
         #處理調整合理的公告日，檢查用
         fin_data['ndate'] = fin_data['mdate'] + pandas.DateOffset(months=6)
 
     if rename_cols is True:
         for this_code in query_code:
-            accountType = params.accountData.loc[params.accountData['code']==this_code,'cgrp'].values[0]
-            accountCname = params.accountData.loc[params.accountData['code']==this_code,'cname'].values[0]
+            accountType = params.accountData.loc[params.accountData['code']==this_code, 'cgrp'].values[0]
+            accountCname = params.accountData.loc[params.accountData['code']==this_code, 'cname'].values[0]
             fin_data[this_code] = fin_data[this_code].fillna(0)
             fin_data = fin_data.rename(index=str, columns={this_code: accountCname})
-            params.indicator_attr[this_code] = {'name':accountCname,'frequency':4}
-         #全部查完 開始合併
-    print('成功查詢會計家數:'+str(len(fin_data['coid'].unique())))
-    return  fin_data        
-
-def get_active_report(*,query_code:list = [],query_coid:list = [],
+            params.indicator_attr[this_code] = {'name':accountCname, 'frequency':4}
+         
+            #全部查完 開始合併
+    print('成功查詢會計家數:' + str(len(fin_data['coid'].unique())))
+    return  fin_data       
+ 
+# 輸入: query_code(會計科目代碼) query_coid(股票代碼)  可以產出 findata_all (產出結果與do_query函式重複)
+def get_active_report(*, query_code:list = [], query_coid:list = [],
                         sample_dates:list = []):
     params.acc_code = query_code
 
     tejapi.ApiConfig.api_key = api_key
     if len(query_coid) == 0:
         query_coid = params.input_coids.copy()
-    query_column = ['coid','mdate','fin_od']+params.acc_code
+    query_column = ['coid', 'mdate', 'fin_od'] + params.acc_code
 
-    if len(query_coid)==0 or len(query_code)==0 or len(sample_dates)!=2:
+    if len(query_coid) == 0 or len(query_code) == 0 or len(sample_dates) != 2:
         return None
 
     datastart_date = sample_dates[0]
@@ -169,49 +183,51 @@ def get_active_report(*,query_code:list = [],query_coid:list = [],
                   mdate={'gte':findata_cover['mdate'].min(),'lte':findata_cover['mdate'].max()},
                   opts={'columns':query_column,"pivot":True}, paginate=True)
             
-    findata_all = findata_cover.merge(findata_all,on=['coid','mdate','fin_od'],how='left')
-    print('成功查詢重編家數：'+str(len(params.active_list)))
+    findata_all = findata_cover.merge(findata_all,on=['coid', 'mdate', 'fin_od'], how='left')
+    print('成功查詢重編家數：' + str(len(params.active_list)))
         
-    #金融業因為無版本別，直接以統一版處理
+    # 金融業因為無版本別，直接以統一版處理
     query_transfet_list = []
     new_code_list = []
-    for i in range(0,len(params.transfer_acccode_list)):
+    for i in range(0, len(params.transfer_acccode_list)):
         if params.transfer_acccode_list[i]['new_acc_code'] in params.acc_code:
-            query_transfet_list = query_transfet_list+[params.transfer_acccode_list[i]['acc_code'] ]
-            new_code_list = new_code_list+[params.transfer_acccode_list[i]['new_acc_code']]
+            query_transfet_list = query_transfet_list + [params.transfer_acccode_list[i]['acc_code'] ]
+            new_code_list = new_code_list + [params.transfer_acccode_list[i]['new_acc_code']]
     
-    #改為以query_coid與actual_ciod做補集
+    # 改為以query_coid與actual_ciod做補集
     not_active_list = numpy.setdiff1d(params.input_coids, params.active_list).tolist()
-    print('需檢查非重編家數：'+str(len(not_active_list)))
-    if len(params.not_active_list)>0 and len(query_transfet_list)>0:
+    print('需檢查非重編家數：' + str(len(not_active_list)))
+    if len(params.not_active_list) > 0 and len(query_transfet_list) > 0:
         findata_not_active = get_report(query_code=query_transfet_list,
                                         query_coid=params.not_active_list,
                                         sample_dates=sample_dates,
                                         rename_cols=False)
         findata_not_active = findata_not_active.fillna(0)
-        for i in range(0,len(query_transfet_list)):
+        for i in range(0, len(query_transfet_list)):
             findata_not_active = findata_all.rename(index=str, columns={query_transfet_list[i]: new_code_list[i]})
             findata_not_active['fin_od'] = 1
-        findata_all = findata_all.append(findata_not_active,sort=False)
-    #型別處理
+        findata_all = findata_all.append(findata_not_active, sort=False)
+    # 型別處理
     findata_all['semester'] = (findata_all['mdate'].dt.strftime('%m').astype(int)/3).astype(int)
     findata_all['zdate'] =  findata_all['zdate'].astype(str).astype('datetime64')
     findata_all['mdate'] =   findata_all['mdate'].dt.strftime('%Y-%m-%d').astype('datetime64')
-    #排序方便合併
-    findata_all = findata_all.sort_values(by=['coid','mdate','fin_od'])
+    
+    # 排序資料方便合併
+    findata_all = findata_all.sort_values(by=['coid', 'mdate', 'fin_od'])
     params.acc_code_name = []
     for this_code in params.acc_code:
-        accountType = params.activeAccountData.loc[params.activeAccountData['acct_code']==this_code,'acct_type'].values[0]
-        accountCname = params.activeAccountData.loc[params.activeAccountData['acct_code']==this_code,'cdesc'].values[0]
+        accountType = params.activeAccountData.loc[params.activeAccountData['acct_code']==this_code, 'acct_type'].values[0]
+        accountCname = params.activeAccountData.loc[params.activeAccountData['acct_code']==this_code, 'cdesc'].values[0]
         findata_all[this_code] = findata_all[this_code].fillna(0)
         findata_all = findata_all.rename(index=str, columns={this_code: accountCname})
-        params.indicator_attr[this_code] = {'name':accountCname,'frequency':4}
+        params.indicator_attr[this_code] = {'name':accountCname, 'frequency':4}
         params.acc_code_name = params.acc_code_name + [accountCname]
-    print('成功查詢會計家數:'+str(len(findata_all['coid'].unique())))
+    print('成功查詢會計家數:' + str(len(findata_all['coid'].unique())))
     return findata_all
-        
-def do_query(*,query_code:list = [],query_coid:list = [],sample_dates:list = [],
-               query_length:int = 365,active_view:bool = False):
+
+# 輸入 query_code(會計科目代碼) query_length(365)，可以產出: findata_all         
+def do_query(*, query_code:list = [], query_coid:list = [], sample_dates:list = [],
+               query_length:int = 365, active_view:bool = False):
     acc_code_set = set(query_code)
     query_code = list(acc_code_set)
     params.acc_code = query_code
@@ -221,11 +237,11 @@ def do_query(*,query_code:list = [],query_coid:list = [],sample_dates:list = [],
         query_coid = params.input_coids.copy()
 
         #查詢績效指標報酬率    
-    if len(query_coid)==0 or len(query_code)==0 or len(sample_dates)!=2:
+    if len(query_coid) == 0 or len(query_code) == 0 or len(sample_dates) != 2:
         return None
 
-            #先查詢財報公告日，要已公告日為主要日期
-            #查詢季報
+    #先查詢財報公告日，要已公告日為主要日期
+    #查詢季報
     if active_view is False:
         params.active_view = False
         findata_all = get_report(query_code=params.acc_code,
@@ -236,9 +252,10 @@ def do_query(*,query_code:list = [],query_coid:list = [],sample_dates:list = [],
         findata_all = get_active_report(query_code=params.acc_code,
                                         query_coid=query_coid,
                                         sample_dates=sample_dates)
+    # 轉換日期格式及排序 
     findata_all['mdate'] = findata_all['mdate'].astype(str)
     all_mdate_list = findata_all['mdate'].unique().astype('datetime64')
     params.all_mdate_list = numpy.sort(all_mdate_list)
-    params.current_mdate = all_mdate_list[len(all_mdate_list)-1] 
+    params.current_mdate = all_mdate_list[len(all_mdate_list) - 1] 
 
     return findata_all
